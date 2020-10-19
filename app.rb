43656126031f1c4833e894ccfe40b34c17d2760f
fentyhall <fentymhall@gmail.com>
2020-10-18 20:30:53 -0700

    Done.

--- app.rb
+++ app.rb
@@ -7,19 +7,33 @@ module FormsLab
       erb :root
     end 
 
-    get '/new' do 
+    get 'pirates' do 
+      @pirates = Pirate.all 
+
+      erb :'pirates/index'
+    end 
+
+    get 'pirates/new' do 
       erb :'pirates/new' 
     end 
 
-    post '/pirates' do 
-      @pirate = Pirate.new(params[:pirate])
-
-      params[:pirate][:ships].each do |ship_details|
-        Ship.new(ship_details)
-      end 
-      @ships = Ship.all
+    get '/pirates/:id' do 
+      @pirate = Pirate.find(params[:id])
 
       erb :'pirates/show'
+    end 
+    
+    post '/pirates' do 
+      pirate = Pirate.new(params[:pirate])
+      
+      params[:pirate][:ships].each do |ship_details|
+        ship = Ship.new(ship_details)
+        ship.pirate = pirate 
+        ship.save
+      end  
+  
+      redirect to 'pirates/#{pirate.id}'
+      # go to localhost:9393/pirates/this pirate's id
     end 
   end
 end
