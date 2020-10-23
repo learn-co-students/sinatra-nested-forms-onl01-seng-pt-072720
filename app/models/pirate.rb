c39057182b56aaaf2118157676559170a4fc4e96
Wayne del Corral Jr <wdelcorjr@gmail.com>
2020-10-23 14:49:02 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,10 @@
 class Pirate
+    attr_accessor :name,  :weight, :height
+
+    def initialize(pirate_hash)
+        @name = pirate_hash[:name]
+        @weight = pirate_hash[:weight]
+        @height = pirate_hash[:height]
+    end
+
 end
