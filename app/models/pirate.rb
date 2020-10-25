cbe2bf3290b30320a1fdf78d926401cf3b9e5970
yani82 <yani.iman1@gmail.com>
2020-10-24 23:36:49 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -4,17 +4,17 @@ class Pirate # <ActiveRecord::Base
 
     attr_accessor :name, :weight, :height 
 
-    PIRATES = []
+    @@all = []
 
     def initialize(params)
         @name = params[:name]
         @weight = params[:weight]
         @height = params[:height] 
-        PIRATES << self 
+        @@all << self 
     end 
 
     def self.all
-        PIRATES
+        @@all
     end 
 
 end
