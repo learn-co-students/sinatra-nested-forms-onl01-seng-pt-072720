7bbc15b7e5cfdfb40663f0d273840b22be49d028
aarongoen <aarongoen2.0@gmail.com>
2020-10-28 00:13:32 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,16 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
-
+  get '/' do
+    "Welcome to the Nested Forms Lab!"
   end
+
+  get '/new' do
+    erb :'pirates/new'
+  end
+
+  post '/pirates' do
+    erb :'pirates/show'
+  end
+end
 end
